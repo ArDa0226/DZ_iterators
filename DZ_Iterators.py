166ea4b35d1c814e7ea3c5acb9caefd82662c73d
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-05-06 15:15:06 +0500

    ДЗ:_по_теме_Итераторы
    Версия 1.0

--- DZ_Iterators.py
+++ DZ_Iterators.py
@@ -1,21 +1,30 @@
 
 class EvenNumbers:
 
-    def __init__(self, start=0, end=1 ):
-        self.start, self.end = start, end
+    def __init__(self, start=0, end=1):
+        self.start = start
+        self.end = end
+        self.step = 1
+
 
     def __iter__(self):
+        self.value = self.start - self.step
         return self
-
     def __next__(self):
-        if self.start >= self.end:
-            raise StopIteration
-        else:
-            self.start += 2
-            return self.start - 2
+        self.start += self.step
+        if self.start > 1:
+            if self.start > self.end:
+                raise StopIteration
+        if self.start % 2 == 0:
+            return self.start
 
-en = EvenNumbers(10,25)
+
+en = EvenNumbers(start=10,end=25)
 for i in en:
     print(i)
 
+
+# for i in range(10, 25):
+#     if i % 2 == 0:
+#         print(i)
 
